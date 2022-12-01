0bbdfded562119ca9a0ef315bbe8b41b6e319ea0
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-12-01 02:31:49 -0800

    if else loop unctions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
 
+
+def uppercase(str):
+    fori in range(len(str)):
+        uni_code = ord(str[i[)
+        if uni_code >= 97 and uni_code <= 122:
+            uni_code = uni_code -32
+        print("{}".format(chr(uni_code)), end='')
+    print()
