75af62e0a41f649218c60a715c73222e977e4341
Victoria-imaobong Solomon <solomonvictoria2002@gmail.com>
2022-12-04 19:10:47 -0800

    import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,9 +4,9 @@
 
 if __name__ == "__main__":
     """program that prints all the names defined by hidden_4 module."""
-    import hidden_4
+    import hidden_4.pyc
 
-    names = dir(hidden_4)
-    gor name in names:
-        if name[:2} != "__":
+    names = dir(hidden_4.pyc)
+    for name in names:
+        if name[:2] != "__":
             print(name)
